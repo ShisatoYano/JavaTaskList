cf2927e6ee5f0db8bc21d5ae6ccc3291061dccfb
Shisato <shisatoyano@gmail.com>
2022-04-03 22:32:58 +0900

    first commit

---
+++ src/main/java/jp/shisato/javaproject/javatasklist/JavaTaskListApplication.java
@@ -0,0 +1,13 @@
+package jp.shisato.javaproject.javatasklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTaskListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTaskListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/shisato/javaproject/javatasklist/JavaTaskListApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.shisato.javaproject.javatasklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTaskListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

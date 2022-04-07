f587b0cef4b74dfd8f0b49d94d0a8d3759345982
Shisato <shisatoyano@gmail.com>
2022-04-07 23:18:42 +0900

    implemented helloWorld() method

--- src/main/java/jp/shisato/javaproject/javatasklist/HomeRestController.java
+++ src/main/java/jp/shisato/javaproject/javatasklist/HomeRestController.java
@@ -1,4 +1,15 @@
 package jp.shisato.javaproject.javatasklist;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDateTime;
+
+@RestController
 public class HomeRestController {
+    String helloWorld() {
+        return """
+                Hello world!!
+                Current time is %s.
+                """.formatted(LocalDateTime.now());
+    }
 }

5f4f57ebde5158a799de10f69c199661de79d22e
Shisato <shisatoyano@gmail.com>
2022-04-09 15:19:04 +0900

    modified response string message and implemented unit test codes

--- src/main/java/jp/shisato/javaproject/javatasklist/HomeRestController.java
+++ src/main/java/jp/shisato/javaproject/javatasklist/HomeRestController.java
@@ -1,16 +1,14 @@
 package jp.shisato.javaproject.javatasklist;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
 
 @RestController
 public class HomeRestController {
-    @RequestMapping(value = "/resthello")
+    @RequestMapping(value = "/resthello", method = RequestMethod.GET)
     String helloWorld() {
-        return """
-                Hello world!!
-                Current time is %s.
-                """.formatted(LocalDateTime.now());
+        return "Hello world!!";
     }
 }
---
+++ src/test/java/jp/shisato/javaproject/javatasklist/HomeRestControllerTest.java
@@ -0,0 +1,33 @@
+package jp.shisato.javaproject.javatasklist;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+class HomeRestControllerTest {
+    private MockMvc mockMvc; // Mock of Spring MVC
+
+    @BeforeEach
+    void settingMock() {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(new HomeRestController()).build();
+    }
+
+    @Test
+    void requestByGet() throws Exception {
+        mockMvc.perform(get("/resthello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello world!!"));
+    }
+
+    @Test
+    void requestByPost() throws Exception {
+        mockMvc.perform(post("/resthello"))
+                .andExpect(status().isMethodNotAllowed());
+    }
+}

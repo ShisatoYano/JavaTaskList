e517ebcb567a1c598c31e4355494aae84654290d
Shisato <shisatoyano@gmail.com>
2022-04-10 22:57:33 +0900

    implemented HomeController class

--- src/main/java/jp/shisato/javaproject/javatasklist/HomeController.java
+++ src/main/java/jp/shisato/javaproject/javatasklist/HomeController.java
@@ -1,4 +1,32 @@
 package jp.shisato.javaproject.javatasklist;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDateTime;
+
+@Controller
 public class HomeController {
+    private String currentDateTime;
+
+    HomeController() {
+        currentDateTime = LocalDateTime.now().toString();
+    }
+
+    public void setDateTime(String dateTime) { currentDateTime = dateTime; }
+
+    @RequestMapping(value = "/hello")
+    @ResponseBody
+    String helloWorld() {
+        return """
+                <html>
+                    <head><title>Hello world!!</title></head>
+                    <body>
+                        <h1>Hello world!!</h1>
+                        Current time is %s.
+                    </body>
+                </html>
+                """.formatted(currentDateTime);
+    }
 }

becdff648b605ba2de96663426b883914e8b9f66
Shisato <shisatoyano@gmail.com>
2022-04-10 20:46:08 +0900

    refactored by using TaskItems class

--- src/main/java/jp/shisato/javaproject/javatasklist/HomeRestController.java
+++ src/main/java/jp/shisato/javaproject/javatasklist/HomeRestController.java
@@ -1,26 +1,25 @@
 package jp.shisato.javaproject.javatasklist;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-import java.util.stream.Collectors;
 
 @RestController
 public class HomeRestController {
     private String currentDateTime;
-
-    record TaskItem(String id, String task, String deadline, boolean done) {}
-    private List<TaskItem> taskItems = new ArrayList<>();
+    private TaskItems taskItems;
 
     HomeRestController() {
-        this.currentDateTime = LocalDateTime.now().toString();
+        currentDateTime = LocalDateTime.now().toString();
+        taskItems = new TaskItems();
     }
 
     public void setDateTime(String dateTime) {
-        this.currentDateTime = dateTime;
+        currentDateTime = dateTime;
     }
 
     @RequestMapping(value = "/resthello", method = RequestMethod.GET)
@@ -28,25 +27,18 @@ public class HomeRestController {
         return """
                 Hello world!!
                 Current time is %s.
-                """.formatted(this.currentDateTime);
+                """.formatted(currentDateTime);
     }
 
     @GetMapping(value = "/restadd")
     String addItem(@RequestParam("task") String task,
                    @RequestParam("deadline") String deadline) {
-        String id = UUID.randomUUID().toString().substring(0, 8);
-        TaskItem item = new TaskItem(id, task, deadline, false);
-        taskItems.add(item);
-
+        taskItems.add(task, deadline);
         return "Task was added successfully.";
     }
 
     @GetMapping(value = "/restlist")
     String listItems() {
-        String result = taskItems.stream()
-                .map(TaskItem::toString)
-                .collect(Collectors.joining(", "));
-
-        return result;
+        return taskItems.itemList();
     }
 }

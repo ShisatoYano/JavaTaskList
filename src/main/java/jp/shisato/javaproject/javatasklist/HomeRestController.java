4f60eb3a1a53f07fa812ac02cb2edb717879d65f
Shisato <shisatoyano@gmail.com>
2022-04-10 14:38:06 +0900

    implemented addItem method to add task

--- src/main/java/jp/shisato/javaproject/javatasklist/HomeRestController.java
+++ src/main/java/jp/shisato/javaproject/javatasklist/HomeRestController.java
@@ -1,14 +1,18 @@
 package jp.shisato.javaproject.javatasklist;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 public class HomeRestController {
     private String currentDateTime;
+
+    record TaskItem(String id, String task, String deadline, boolean done) {}
+    private List<TaskItem> taskItems = new ArrayList<>();
 
     HomeRestController() {
         this.currentDateTime = LocalDateTime.now().toString();
@@ -24,5 +28,15 @@ public class HomeRestController {
                 Hello world!!
                 Current time is %s.
                 """.formatted(this.currentDateTime);
+    }
+
+    @GetMapping(value = "/restadd")
+    String addItem(@RequestParam("task") String taks,
+                   @RequestParam("deadline") String deadline) {
+        String id = UUID.randomUUID().toString().substring(0, 8);
+        TaskItem item = new TaskItem(id, taks, deadline, false);
+        taskItems.add(item);
+
+        return "Task was added successfully.";
     }
 }

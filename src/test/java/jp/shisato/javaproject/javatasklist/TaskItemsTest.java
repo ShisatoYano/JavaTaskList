cd62f917beebf9ac16b770c121bbd693a4d52267
Shisato <shisatoyano@gmail.com>
2022-04-26 23:13:27 +0900

    implemented TaskListDao

--- src/test/java/jp/shisato/javaproject/javatasklist/TaskItemsTest.java
+++ src/test/java/jp/shisato/javaproject/javatasklist/TaskItemsTest.java
@@ -2,8 +2,6 @@ package jp.shisato.javaproject.javatasklist;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class TaskItemsTest {
